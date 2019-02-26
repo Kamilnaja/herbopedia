d68bc27245690d525c88023dd182958acf9e76a3
najak <kamilnaja@gmail.com>
2019-02-26 12:20:16 +0100

    added Herb model

--- src/app/herblist/herblist.component.ts
+++ src/app/herblist/herblist.component.ts
@@ -1,29 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { HerbsService } from '.././herbs.service';
+import { Herb } from 'models/Herb';
 
 @Component({
   selector: 'app-herblist',
   templateUrl: './herblist.component.html',
 })
 export class HerblistComponent implements OnInit {
-
-  herbs:any = [];
-  selectedHerb;
+  herbs: Herb[] = [];
+  selectedHerb: Herb;
   isList = true;
-  constructor(private _herbService: HerbsService){}
+  isClassVisible = false;
 
-  isClassVisible: false;
+  constructor(private _herbService: HerbsService) { }
 
   ngOnInit() {
     this._herbService.getHerbs()
-      .subscribe(resHerbsData => this.herbs = resHerbsData);
+      .subscribe(res => this.herbs = res);
   }
 
   onSelect(herb) {
     this.selectedHerb = herb;
   }
 
-  toggleCSS () {
+  toggleCSS() {
     console.log(this.isList);
   }
 
---
+++ src/models/Herb.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class Herb {
+    img: string;
+    name: string;
+    howWork: string[]
+}
