6013ff83b1b8b9185694305176728186a2364cf2
najak <kamilnaja@gmail.com>
2019-03-23 08:16:28 +0100

    added headless config for Karma

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,9 @@
 import { TestBed, async } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { HerbsService } from './herbs.service';
+import { HttpModule } from '@angular/http';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -8,6 +11,8 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      imports: [RouterModule, HttpModule],
+      providers: [HerbsService]
     }).compileComponents();
   }));
 
